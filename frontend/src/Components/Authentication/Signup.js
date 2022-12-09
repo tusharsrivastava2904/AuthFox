98e6081efe32a2bccd49af2775fda0167fe1986d
Tushar Srivastava <tusharsrivastava2904@gmail.com>
2022-12-09 13:11:13 +0530

    signup validations updated

--- frontend/src/Components/Authentication/Signup.js
+++ frontend/src/Components/Authentication/Signup.js
@@ -1,5 +1,7 @@
-import { FormControl, FormLabel, VStack, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
-import React, {useState} from 'react'
+import { FormControl, FormLabel, VStack, Input, InputGroup, InputRightElement, Button, useToast } from '@chakra-ui/react';
+import React, {useState} from 'react';
+import axios from "axios";
+import {useHistory} from "react-router-dom";
 
 const Signup = () => {
 
@@ -10,12 +12,111 @@ const Signup = () => {
     const [password, setPassword] = useState();
     const [confirmPassword, setConfirmPassword] = useState();
     const [pic, setPic] = useState();
+    const [loading, setLoading] = useState(false);
 
+    const history = useHistory();
+    
+    const toast = useToast();
     const handleClick = () => setShow(!show);
     const handleClickConfirmPassword = () => setShowCP(!showCP);
 
-    const postDetails = (pics) => {};
-    const submitHandler = (pics) => {};
+    const postDetails = (pics) => {
+      setLoading(true);
+      if(pics===undefined) {
+        toast({
+          title: "Please select an Image",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        return;
+      }
+
+      if (pics.type==="image/jpeg" || pics.type==="image/png"){
+        const data = new FormData();
+        data.append("file", pics);
+        data.append("upload_preset", "authfox");
+        data.append("cloud_name", "dvslhds5w");
+        fetch("https://api.cloudinary.com/v1_1/dvslhds5w/image/upload", {
+          method: "post",
+          body: data,
+        }).then((res) => res.json())
+          .then((data) => {
+           setPic(data.url.toString());
+           console.log(data.url.toString());
+           setLoading(false);
+        }).catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+      } else {
+        toast({
+          title: "Please select an Image",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        setLoading(false);
+        return;
+      }
+    };
+    const submitHandler = async () => {
+      setLoading(true);
+      if(!name || !email || !password || !confirmPassword){
+        toast({
+          title: "Please fill all the fields!",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        setLoading(false);
+        return;
+      } if (password!==confirmPassword){
+        toast({
+          title: "Passwords do not match!",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          }
+        };
+        const {data} = await axios.post("api/user", {name, email, password, pic}, config);
+        toast({
+          title: "Signed-Up Successfully",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setLoading(false);
+        history.push('/chats')
+      } catch (error) {
+        toast({
+          title: "Error Occured!",
+          description: error.response.data.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        setLoading(false);
+      }
+
+    };
 
 
   return (
@@ -84,6 +185,7 @@ const Signup = () => {
         color="white"
         style={{marginTop: 15}}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign-Up
       </Button>
